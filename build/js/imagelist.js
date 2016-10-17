5a89e575777d89a1ccce32affdda9b43a73cdf9e
zarinanorton <norton.zarina@gmail.com>
2016-10-17 11:58:14 -0400

    added an image-list ability to eventscreate.html

---
+++ build/js/imagelist.js
@@ -0,0 +1,25 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+              $('<li class="imageWrap"></li>')
+                .append(
+                $('<img></img>') //Equivalent: $(document.createElement('img'))
+                  .attr('src', e.target.result)
+                  .width(200)
+                  .height(150)
+                )
+                .append(
+                  $('<button></button>"')
+                  .attr('class', "btn default image-button")
+                  .append(
+                    $('<span></span>')
+                    .attr('class','glyphicon glyphicon-trash')
+                  )
+                )
+                .appendTo('#imagelist');
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
