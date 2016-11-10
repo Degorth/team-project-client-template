e4ea5cb13225452152f5c114c3f36efbd163f1cd
James Coyne <james.coyne@gmail.com>
2016-11-10 13:53:51 -0500

    Added database.js

---
+++ database/database.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var data ={
+  "Events": {
+    "1": {
+      "_id": 1,
+      "eventName": "Umass Shotokan Karate Club",
+      "eventDate": 1478803334000,
+      "eventDesc": "The Umass Shotokan Karate Club is a long standing branch of the\nInternation Shotokan Karate Federation. We are the 6th club founded\nthough the organization in the United States and have been at this\nuniversity for over 40 years!"
+      "eventOwner": 1,
+      ""
+    }
+  }
+}
