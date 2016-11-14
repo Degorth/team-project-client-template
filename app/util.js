aa3884012a86d02c058ad5981caa244ca4b7b586
Henry Ecker <hecker@umass.edu>
2016-11-14 15:59:13 -0500

    Updated unixTime Display in util.js

--- app/util.js
+++ app/util.js
@@ -1,3 +1,7 @@
 export function unixTimeToString(time) {
-    return new Date(time).toLocaleString();
+    return new Date(time).toLocaleString().substring(0,17);
+}
+
+export function unixTimeIsToday(time){
+  return(new Date(time).toLocaleString().substring(0,10) === new Date().getTime().toLocaleString().substring(0,10));
 }
