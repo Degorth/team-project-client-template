e4aff8b65695f326eb2196813561102dc7e52cfe
Henry Ecker <hecker@umass.edu>
2016-11-13 19:14:09 -0500

    Added mount location to index.html and added router to app.js

--- app/app.js
+++ app/app.js
@@ -1,1 +1,19 @@
-/* Empty for now. :) */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {IndexRoute, Router, Route, browserHistory} from 'react-router';
+
+
+class App extends React.Component {
+    render() {
+        return (
+          <div />
+        )
+    }
+}
+
+ReactDOM.render((
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+        </Route>
+    </Router>
+), document.getElementById('ugo-app'));
