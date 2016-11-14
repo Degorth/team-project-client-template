bd813e57d9acb2c2155983f0c5c16abda30678cb
Henry Ecker <hecker@umass.edu>
2016-11-14 13:58:54 -0500

    made the navbar visable w/o styling. Added Schedule with state flow

--- app/app.js
+++ app/app.js
@@ -3,30 +3,21 @@ import ReactDOM from 'react-dom';
 import {IndexRoute, Link, Router, Route, browserHistory} from 'react-router';
 
 import bored from './components/bored.js';
-import schedule from './components/schedule.js';
-import upcoming from './components/upcoming.js';
+import Schedule from './components/schedule.js';
+//import upcoming from './components/upcoming.js';
 import homepage from './components/homepage.js';
 import eventssearch from './components/eventssearch.js';
 import eventscreate from './components/eventscreate.js';
-import profileuser from './components/profileuser.js';
+//import profileuser from './components/profileuser.js';
 
-ReactDOM.render((
-    <Router history={browserHistory}>
-      <Route path='/' component={Container}>
-        <IndexRoute component={homepage} />
-        <Route path='/schedule' component={schedule}></Route>
-        <Route path='/upcoming' component={upcoming}></Route>
-        <Route path='/bored' component={bored}></Route>
-        <Route path='/eventssearch' component={eventssearch}></Route>
-        <Route path='/eventscreate' component={eventscreate}></Route>
-        <Route path='/profileuser' component={profileuser}></Route>
-      </Route>
-    </Router>
-), document.getElementById('ugo-app'));
-
-const navbar = () => (
+class ScheduleContainer extends React.Component{
+  render(){
+    return(<Schedule user={1}/>);
+  }
+}
+const Navbar = () => (
   <div>
-    <Link to='/'>Home</Link>&nbsp;
+    <Link to='/'>Home</Link>
     <Link to='/schedule'>My Scheule</Link>
     <Link to='/upcoming'>Upcoming</Link>
     <Link to='/bored'>I'm Bored</Link>
@@ -35,8 +26,40 @@ const navbar = () => (
     <Link to='/eventscreate'>Create A New Event</Link>
   </div>
 )
-
-const Container = (props) => <div>
-  <navbar />
-  {props.children}
-</div>
+//<Link to='/'>Home</Link>&nbsp;
+class Container extends React.Component {
+  render(){
+    return(
+      <div>
+        <Navbar />
+        {this.props.children}
+      </div>
+    )
+  }
+}
+ReactDOM.render((
+    <Router history={browserHistory}>
+      <Route path='/' component={Container}>
+        <IndexRoute component={homepage} />
+        <Route path='/schedule' component={ScheduleContainer}></Route>
+        <Route path='/bored' component={bored}></Route>
+        <Route path='/eventssearch' component={eventssearch}></Route>
+        <Route path='/eventscreate' component={eventscreate}></Route>
+      </Route>
+    </Router>
+), document.getElementById('ugo-app'));
+/*
+ReactDOM.render((
+    <Router history={browserHistory}>
+      <Route path='/' component={Container}>
+        <IndexRoute component={homepage} />
+        <Route path='/upcoming' component={upcoming}></Route>
+        <Route path='/schedule' component={ScheduleContainer}></Route>
+        <Route path='/bored' component={bored}></Route>
+        <Route path='/eventssearch' component={eventssearch}></Route>
+        <Route path='/eventscreate' component={eventscreate}></Route>
+        <Route path='/profileuser' component={profileuser}></Route>
+      </Route>
+    </Router>
+), document.getElementById('ugo-app'));
+*/
