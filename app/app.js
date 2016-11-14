4cce7a2831a14081bb14836e435f588e7f517c89
Henry Ecker <hecker@umass.edu>
2016-11-14 18:08:38 -0500

    Updated Navbar to include styling

--- app/app.js
+++ app/app.js
@@ -11,80 +11,110 @@ import EventsCreate from './components/eventscreate.js';
 //import profileuser from './components/profileuser.js';
 import UnimplementedReactComponent from './components/UnimplementedReactComponent.js';
 
-class boredContainer extends React.Component{
-  render(){
-    return(<Bored />);
-  }
+class boredContainer extends React.Component {
+    render() {
+        return (<Bored/>);
+    }
 }
 
-class ScheduleContainer extends React.Component{
-  render(){
-    return(<Schedule user={1}/>);
-  }
+class ScheduleContainer extends React.Component {
+    render() {
+        return (<Schedule user={1}/>);
+    }
 }
 
-class upcomingContainer extends React.Component{
-  render(){
-    return(<UnimplementedReactComponent/>);
-  }
+class upcomingContainer extends React.Component {
+    render() {
+        return (<UnimplementedReactComponent/>);
+    }
 }
 
-class homepageContainer extends React.Component{
-  render(){
-    return(<Homepage/>);
-  }
+class homepageContainer extends React.Component {
+    render() {
+        return (<Homepage/>);
+    }
 }
 
-class eventssearchContainer extends React.Component{
-  render(){
-    return(<EventsSearch/>);
-  }
+class eventssearchContainer extends React.Component {
+    render() {
+        return (<EventsSearch/>);
+    }
 }
 
-class eventscreateContainer extends React.Component{
-  render(){
-    return(<EventsCreate/>);
-  }
+class eventscreateContainer extends React.Component {
+    render() {
+        return (<EventsCreate/>);
+    }
 }
 
-class profileuserContainer extends React.Component{
-  render(){
-    return(<UnimplementedReactComponent/>);
-  }
+class profileuserContainer extends React.Component {
+    render() {
+        return (<UnimplementedReactComponent/>);
+    }
 }
-const Navbar = () => (
-  <div>
-    <Link to='/'>Home</Link>
-    <Link to='/schedule'>My Scheule</Link>
-    <Link to='/upcoming'>Upcoming</Link>
-    <Link to='/bored'>I'm Bored</Link>
-    <Link to='/eventssearch'>Events</Link>
-    <Link to='/profileuser'>My Profile</Link>
-    <Link to='/eventscreate'>Create A New Event</Link>
-  </div>
-)
+class Navbar extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <a className="navbar-brand" href="#">UGOEvent</a>
+                    </div>
+                    <ul className="nav navbar-nav">
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/schedule'>My Scheule</Link>
+                        </li>
+
+                        <li>
+                            <Link to='/upcoming'>Upcoming</Link>
+                        </li>
+                        <li>
+                            <Link to='/bored'>I'm Bored</Link>
+                        </li>
+                        <li>
+                            <Link to='/eventssearch'>Events</Link>
+                        </li>
+                    </ul>
+                    <div className="btn-group-vertical pull-right" role="group">
+                        <Link className="btn btn-primary" to='/profileuser'>
+                            <span className="glyphicon glyphicon-user"></span>
+                            My Profile</Link>
+                        <Link className="btn btn-primary" to='/eventscreate'>
+                            <span className="glyphicon glyphicon-plus"></span>Create A New Event</Link>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
 //<Link to='/'>Home</Link>&nbsp;
+/*
+const Navbar = () => (
+)
+*/
 class Container extends React.Component {
-  render(){
-    return(
-      <div>
-        <Navbar />
-        {this.props.children}
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Navbar/> {this.props.children}
+            </div>
+        )
+    }
 }
 
 ReactDOM.render((
     <Router history={browserHistory}>
-      <Route path='/' component={Container}>
-        <IndexRoute component={homepageContainer} />
-        <Route path='/upcoming' component={upcomingContainer}></Route>
-        <Route path='/schedule' component={ScheduleContainer}></Route>
-        <Route path='/bored' component={boredContainer}></Route>
-        <Route path='/eventssearch' component={eventssearchContainer}></Route>
-        <Route path='/eventscreate' component={eventscreateContainer}></Route>
-        <Route path='/profileuser' component={profileuserContainer}></Route>
-      </Route>
+        <Route path='/' component={Container}>
+            <IndexRoute component={homepageContainer}/>
+            <Route path='/upcoming' component={upcomingContainer}></Route>
+            <Route path='/schedule' component={ScheduleContainer}></Route>
+            <Route path='/bored' component={boredContainer}></Route>
+            <Route path='/eventssearch' component={eventssearchContainer}></Route>
+            <Route path='/eventscreate' component={eventscreateContainer}></Route>
+            <Route path='/profileuser' component={profileuserContainer}></Route>
+        </Route>
     </Router>
 ), document.getElementById('ugo-app'));
