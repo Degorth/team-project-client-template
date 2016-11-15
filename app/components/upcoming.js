ecd00354d997527f2eb822ff1e2d76d79d1c640e
Elitza Neytcheva <eneytcheva@gmail.com>
2016-11-14 23:13:10 -0500

    upcoming.js started

---
+++ app/components/upcoming.js
@@ -0,0 +1,48 @@
+
+
+        <div class="container">
+            <h3>Upcoming Events:</h3>
+            <p> Check out new events coming soon near you </p>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Activity </th>
+                        <th>Organizer <button type="button" class "btn btn-default"><span class ="glyphicon glyphicon-resize-vertical"></span></button></th>
+                        <th>Time <button type="button" class "btn btn-default"><span class ="glyphicon glyphicon-resize-vertical"></span></button></th>
+                        <th>Date <button type="button" class "btn btn-default"><span class ="glyphicon glyphicon-resize-vertical"></span></button></th>
+                        <th>Location <button type="button" class "btn btn-default"><span class ="glyphicon glyphicon-resize-vertical"></span></button></th>
+                        <th>Rating <button type="button" class "btn btn-default"><span class ="glyphicon glyphicon-resize-vertical"></span></button></th>
+                        <th>Description </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Karate Club Meeting</td>
+                        <td>UMass Shotokan Karate Club <img class="card-img-top profile-img" src="img/iskf.jpg" alt="Karate Club img"></td>
+                        <td>6:00pm-7:00pm</td>
+                        <td>Monday October 10th</td>
+                        <td>Boyden Gym Basement Squash Courts</td>
+                        <td>4/5  </td>
+                        <td>A Karate Club meeting  <a href="eventspageinfo.html">see more</a></td>
+                    </tr>
+                    <tr>
+                      <td>Karate Club Meeting</td>
+                      <td>UMass Shotokan Karate Club <img class="card-img-top profile-img" src="img/iskf.jpg" alt="Karate Club img"></td>
+                      <td>6:00pm-7:00pm</td>
+                      <td>Wednesday October 12th</td>
+                      <td>Boyden Gym Basement Squash Courts</td>
+                      <td>4/5</td>
+                      <td>A Karate Club meeting  <a href="eventspageinfo.html">see more</a></td>
+                    </tr>
+                    <tr>
+                      <td>Karate Club Meeting</td>
+                      <td>UMass Shotokan Karate Club <img class="card-img-top profile-img" src="img/iskf.jpg" alt="Karate Club img"></td>
+                      <td>6:00pm-7:00pm</td>
+                      <td>Friday October 14th</td>
+                      <td>Boyden Gym Basement Squash Courts</td>
+                      <td>4/5</td>
+                      <td>A Karate Club meeting  <a href="eventspageinfo.html">see more</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
