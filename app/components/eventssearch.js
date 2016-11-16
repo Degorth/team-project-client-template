48a78da1a57570561953762c2d3032ff884a5cbb
Henry Ecker <hecker@umass.edu>
2016-11-15 21:14:40 -0500

    Search now renders correctly

--- app/components/eventssearch.js
+++ app/components/eventssearch.js
@@ -45,21 +45,23 @@ export default class EventsSearch extends React.Component {
     }
 
     dispResults() {
-      if(this.state.results!==undefined && this.state.results !==null){
-        this.state.results.map((ev) => {
-          return (
-              <div>
-                  <img src="#" width="150px"/>
-                  {ev.name}
-                  <div>{ev.desc}</div>
-                  <a className="btn btn-secondary" href="#" role="button">
-                      <span className="glyphicon glyphicon-hand-up"></span>
-                      View Group Page</a>
-              </div>
-          )
-      });
-      } else {
-        <div>No Events Found </div>
+        if (this.state.results !== undefined && this.state.results !== null) {
+            return (
+                <div className="searchresult">
+                    {this.state.results.map((ev) => {
+                        return (
+                            <div key={ev._id}>
+                                <img src="#" width="150px"/> {ev.name}
+                                <div>{ev.desc}</div>
+                                <a className="btn btn-secondary" href="#" role="button">
+                                    <span className="glyphicon glyphicon-hand-up"></span>
+                                    View Group Page</a>
+                            </div>
+                        )
+                    })}
+                </div>
+            );
+        } else { < div > No Events Found < /div>
       }
     }
 
@@ -68,73 +70,67 @@ export default class EventsSearch extends React.Component {
             <div>
 
                 <div className="row">
-                    <div className="col-md-1"></div>
-                    <div className="col-md-10">
-                        <form>
-                            <div className="form-group">
-                                <input type="text" className="form-control" placeholder="Event/Organization Description" onChange={(e) => this.handleSearch(e.target.value)}/>
-                            </div>
-                            <div className="form-check">
-                                <label htmlFor="form-check">Occurring On</label>
-                                <label className="form-check-label">
-                                    <input type="checkbox" className="form-check-input" onClick={(e) => this.handleChange(e, "Mon")}/>
-                                    Mon
-                                </label>
-                                <label className="form-check-label">
-                                    <input type="checkbox" className="form-check-input" onClick={(e) => this.handleChange(e, "Tue")}/>
-                                    Tue
-                                </label>
-                                <label className="form-check-label">
-                                    <input type="checkbox" className="form-check-input" onClick={(e) => this.handleChange(e, "Wed")}/>
-                                    Wed
-                                </label>
-                                <label className="form-check-label">
-                                    <input type="checkbox" className="form-check-input" onClick={(e) => this.handleChange(e, "Thurs")}/>
-                                    Thurs
-                                </label>
-                                <label className="form-check-label">
-                                    <input type="checkbox" className="form-check-input" onClick={(e) => this.handleChange(e, "Fri")}/>
-                                    Fri
-                                </label>
-                                <label className="form-check-label">
-                                    <input type="checkbox" className="form-check-input" onClick={(e) => this.handleChange(e, "Sat")}/>
-                                    Sat
-                                </label>
-                                <label className="form-check-label">
-                                    <input type="checkbox" className="form-check-input" onClick={(e) => this.handleChange(e, "Sun")}/>
-                                    Sun
-                                </label>
-                            </div>
-                            <div className="form-group row">
-                                <label htmlFor="example-time-input" className="col-xs-2 col-form-label">Occurring After</label>
-                                <div className="col-xs-10">
-                                    <input className="form-control" type="time" onChange={(e) => this.handleAfter(e.target.value)} value={this.state.after} id="example-time-input"/>
-                                </div>
-                            </div>
-                            <div className="form-group row">
-                                <label htmlFor="example-time-input" className="col-xs-2 col-form-label">Before</label>
-                                <div className="col-xs-10">
-                                    <input className="form-control" type="time" onChange={(e) => this.handleBefore(e.target.value)} value={this.state.before} id="example-time-input"/>
-                                </div>
-                            </div>
-                            <button type="submit" className="btn btn-secondary btn-block" onClick={(e) => this.handleSubmit(e)}>Find Events!</button>
-                        </form>
+                    <div className="col-md-1"></div > <div className="col-md-10">
+            <form>
+                <div className="form-group">
+                    <input type="text" className="form-control" placeholder="Event/Organization Description" onChange={(e) => this.handleSearch(e.target.value)}/>
+                </div>
+                <div className="form-check">
+                    <label htmlFor="form-check">Occurring On</label>
+                    <label className="form-check-label">
+                        <input type="checkbox" className="form-check-input" onClick={(e) => this.handleChange(e, "Mon")}/>
+                        Mon
+                    </label>
+                    <label className="form-check-label">
+                        <input type="checkbox" className="form-check-input" onClick={(e) => this.handleChange(e, "Tue")}/>
+                        Tue
+                    </label>
+                    <label className="form-check-label">
+                        <input type="checkbox" className="form-check-input" onClick={(e) => this.handleChange(e, "Wed")}/>
+                        Wed
+                    </label>
+                    <label className="form-check-label">
+                        <input type="checkbox" className="form-check-input" onClick={(e) => this.handleChange(e, "Thurs")}/>
+                        Thurs
+                    </label>
+                    <label className="form-check-label">
+                        <input type="checkbox" className="form-check-input" onClick={(e) => this.handleChange(e, "Fri")}/>
+                        Fri
+                    </label>
+                    <label className="form-check-label">
+                        <input type="checkbox" className="form-check-input" onClick={(e) => this.handleChange(e, "Sat")}/>
+                        Sat
+                    </label>
+                    <label className="form-check-label">
+                        <input type="checkbox" className="form-check-input" onClick={(e) => this.handleChange(e, "Sun")}/>
+                        Sun
+                    </label>
+                </div>
+                <div className="form-group row">
+                    <label htmlFor="example-time-input" className="col-xs-2 col-form-label">Occurring After</label>
+                    <div className="col-xs-10">
+                        <input className="form-control" type="time" onChange={(e) => this.handleAfter(e.target.value)} value={this.state.after} id="example-time-input"/>
                     </div>
                 </div>
-                <hr/>
-                <div className="row">
-                    <div className="col-md-1"></div>
-                    <div className="col-md-10">
-                        <div className="panel panel-default">
-                            <div className="panel-body">
-                                <div className="searchresult">
-                                  {this.dispResults()}
-                                </div>
-                            </div>
-                        </div>
+                <div className="form-group row">
+                    <label htmlFor="example-time-input" className="col-xs-2 col-form-label">Before</label>
+                    <div className="col-xs-10">
+                        <input className="form-control" type="time" onChange={(e) => this.handleBefore(e.target.value)} value={this.state.before} id="example-time-input"/>
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-secondary btn-block" onClick={(e) => this.handleSubmit(e)}>Find Events!</button>
+            </form>
+        </div> < /div>
+                <hr/ > <div className="row">
+            <div className="col-md-1"></div>
+            <div className="col-md-10">
+                <div className="panel panel-default">
+                    <div className="panel-body">
+                        {this.dispResults()}
                     </div>
                 </div>
             </div>
+        </div> < /div>
         );
     }
 }
